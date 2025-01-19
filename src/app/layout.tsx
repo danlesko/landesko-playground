68cab3481d4c3a233c85aa3bc115e14751688a77
Dan Lesko <dalesko@cisco.com>
2025-01-19 14:11:54 -0700

    chore: refactor sidebar with knowledge of path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
-import { Montserrat } from 'next/font/google';
+import { Montserrat } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
-import Image from 'next/image'
+import Image from "next/image";
+import MySidebar from "@/components/MySidebar";
 
-const mont = Montserrat({ weight: ['400', '700'], subsets: ['latin'] });
+const mont = Montserrat({ weight: ["400", "700"], subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Landesko's Playground",
@@ -18,49 +18,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${mont.className} antialiased`}
-      >
+      <body className={`${mont.className} antialiased`}>
         <div className="flex flex-col min-h-screen">
           {/* Navbar with z-index */}
           <nav className="row-span-1 col-span-full bg-gradient-to-r from-purple-700 to-cyan-500 p-4 text-zinc-200 font-bold shadow-zinc-900 shadow-lg z-10">
             <span className="flex items-center space-x-4">
-            <Image src="/slide.png" alt="Landesko's Playground" width="40" height="40" />
-            <span className="pl-1">Landesko's Playground</span>
+              <Image
+                src="/slide.png"
+                alt="Landesko's Playground"
+                width="40"
+                height="40"
+              />
+              <span className="pl-1">Landesko's Playground</span>
             </span>
           </nav>
 
           <div className="flex flex-1">
             {/* Sidebar */}
-            <aside className="bg-zinc-800 p-4 w-64 text-zinc-300">
-            <ul className="space-y-2">
-            <li>
-              <Link href="/" className="block p-2 bg-zinc-800 rounded hover:bg-gray-400 hover:text-zinc-900">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link href="/blog" className="block p-2 bg-zinc-800 rounded hover:bg-gray-400 hover:text-zinc-900">
-                Blog
-              </Link>
-            </li>
-            <li>
-              <Link href="/processing" className="block p-2 bg-zinc-800 rounded hover:bg-gray-400 hover:text-zinc-900">
-                Processing
-              </Link>
-            </li>
-            <li>
-              <Link href="/contact" className="block p-2 bg-zinc-800 rounded hover:bg-gray-400 hover:text-zinc-900">
-                Contact
-              </Link>
-            </li>
-            <li>
-              <Link href="/credits" className="block p-2 bg-zinc-800 rounded hover:bg-gray-400 hover:text-zinc-900">
-                Credits
-              </Link>
-            </li>
-          </ul>
-            </aside>
+            <MySidebar />
 
             {/* Main Content */}
             <main className="flex-1 bg-zinc-900 p-4 text-zinc-300">
---
+++ src/components/MySidebar.tsx
@@ -0,0 +1,38 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+
+const MySidebar = () => {
+  const pathname = usePathname();
+  return (
+    <aside className="bg-zinc-800 p-4 w-64 text-zinc-300">
+      <ul className="space-y-2">
+        {[
+          { href: "/", label: "Home" },
+          { href: "/blog", label: "Blog" },
+          { href: "/processing", label: "Processing" },
+          { href: "/contact", label: "Contact" },
+          { href: "/credits", label: "Credits" },
+        ].map((item) => (
+          <li key={item.href}>
+            <Link
+              href={item.href}
+              className={clsx(
+                "block p-2 rounded hover:bg-gray-400 hover:text-zinc-900 focus:bg-purple-600",
+                {
+                  "bg-purple-600": pathname === item.href,
+                  "bg-zinc-800": pathname !== item.href,
+                },
+              )}
+            >
+              {item.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+};
+
+export default MySidebar;
