1b26dac41db387627958c3fbba4ffc4678d301c7
Dan Lesko <dalesko@cisco.com>
2025-01-18 22:39:47 -0700

    chore: see if lazygit works

--- src/app/credits/page.tsx
+++ src/app/credits/page.tsx
@@ -6,7 +6,6 @@ export default function Credits() {
         Thanks to the following resources for helping me build this site:
       </p>
       <ul className="list-disc ml-8">
-
         <li>
         <a className="text-blue-600 hover:text-blue-800 visited:text-purple-600" href="https://www.flaticon.com/free-icon/slider_3264933?term=playground+slide&page=1&position=7&origin=tag&related_id=3264933" title="kid and baby icons">Kid and baby icons created by flatart_icons - Flaticon</a>
         </li>
