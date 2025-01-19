616ef0f1e6da16cb1fa71d01990f2c49242bc371
Dan Lesko <dalesko@cisco.com>
2025-01-19 02:31:54 -0700

    chore: does this work w/ husky?

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,11 @@
-'use client';
+"use client";
 import { useState, useEffect } from "react";
-import {Textarea} from "@rewind-ui/core";
+import { Textarea } from "@rewind-ui/core";
 
 export default function Contact() {
   const [contactValue, setContactValue] = useState<string | null>(null);
 
-  useEffect(()=> {
+  useEffect(() => {
     console.log(contactValue);
   }, [contactValue]);
 
@@ -13,15 +13,23 @@ export default function Contact() {
     <>
       <h2 className="text-4xl font-bold">Contact</h2>
       <p className="text-lg mt-2">
-        Email me directly at: {" "}
-        <a className="text-blue-600 hover:text-blue-800 visited:text-purple-600"
-          href="mailto:lesko.dan.m@gmail.com">
-            lesko.dan.m@gmail.com
-          </a>
+        Email me directly at:{" "}
+        <a
+          className="text-blue-600 hover:text-blue-800 visited:text-purple-600"
+          href="mailto:lesko.dan.m@gmail.com"
+        >
+          lesko.dan.m@gmail.com
+        </a>
       </p>
       <p className="text-lg mt-2 w-1/2 h-1/2">
         Or through the web here:
-        <Textarea className="bg-zinc-800 text-zinc-300 focus:bg-zinc-800 focus:text-zinc-300 focus:ring-zinc-800 focus:ring-0 focus:ring-offset-0" tone="solid" color="purple" placeholder="Ask me anything!" onChange={(e) => setContactValue(e.target.value)}/>
+        <Textarea
+          className="bg-zinc-800 text-zinc-300 focus:bg-zinc-800 focus:text-zinc-300 focus:ring-zinc-800 focus:ring-0 focus:ring-offset-0"
+          tone="solid"
+          color="purple"
+          placeholder="Ask me anything!"
+          onChange={(e) => setContactValue(e.target.value)}
+        />
       </p>
     </>
   );
