343d7c5f6906da3ad424b9e460118b3f4ad683bd
Dan Lesko <dalesko@cisco.com>
2025-01-18 22:32:05 -0700

    chore: first commit after starter

---
+++ src/app/credits/page.tsx
@@ -0,0 +1,16 @@
+export default function Credits() {
+  return (
+    <>
+      <h2 className="text-4xl font-bold">Credits</h2>
+      <p className="text-lg mt-2">
+        Thanks to the following resources for helping me build this site:
+      </p>
+      <ul className="list-disc ml-8">
+
+        <li>
+        <a className="text-blue-600 hover:text-blue-800 visited:text-purple-600" href="https://www.flaticon.com/free-icon/slider_3264933?term=playground+slide&page=1&position=7&origin=tag&related_id=3264933" title="kid and baby icons">Kid and baby icons created by flatart_icons - Flaticon</a>
+        </li>
+        </ul>
+    </>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,12 @@ import type { Metadata } from "next";
 import { Montserrat } from 'next/font/google';
 import "./globals.css";
 import Link from "next/link";
+import Image from 'next/image'
 
 const mont = Montserrat({ weight: ['400', '700'], subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Landesko Playground",
+  title: "Landesko's Playground",
   description: "Dan Lesko's Portfolio Playground and Blog",
 };
 
@@ -23,7 +24,10 @@ export default function RootLayout({
         <div className="flex flex-col min-h-screen">
           {/* Navbar with z-index */}
           <nav className="row-span-1 col-span-full bg-gradient-to-r from-purple-700 to-cyan-500 p-4 text-zinc-200 font-bold shadow-zinc-900 shadow-lg z-10">
-            Landesko's Playground
+            <span className="flex items-center space-x-4">
+            <Image src="/slide.png" alt="Landesko's Playground" width="40" height="40" />
+            <span className="pl-1">Landesko's Playground</span>
+            </span>
           </nav>
 
           <div className="flex flex-1">
@@ -43,6 +47,11 @@ export default function RootLayout({
             <li>
               <Link href="/contact" className="block p-2 bg-zinc-800 rounded hover:bg-gray-400 hover:text-zinc-900">
                 Contact
+              </Link>
+            </li>
+            <li>
+              <Link href="/credits" className="block p-2 bg-zinc-800 rounded hover:bg-gray-400 hover:text-zinc-900">
+                Credits
               </Link>
             </li>
           </ul>
