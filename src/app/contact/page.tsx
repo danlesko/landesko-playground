5a9756cb6af97c272971c988b6dc8e988b81de0c
Dan Lesko <dalesko@cisco.com>
2025-01-19 03:37:56 -0700

    chore: got email to work

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,13 +1,68 @@
 "use client";
-import { useState, useEffect } from "react";
-import { Textarea } from "@rewind-ui/core";
+import { useState, ChangeEvent } from "react";
+import { Textarea, Input, Button } from "@rewind-ui/core";
+import emailjs from "@emailjs/browser";
+
+type Email = {
+  name: string;
+  email: string;
+  message: string;
+};
 
 export default function Contact() {
-  const [contactValue, setContactValue] = useState<string | null>(null);
+  const [userInput, setUserInput] = useState<Email>({
+    name: "",
+    email: "",
+    message: "",
+  });
 
-  useEffect(() => {
-    console.log(contactValue);
-  }, [contactValue]);
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUserInput({
+      ...userInput,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e: Event) => {
+    e.preventDefault();
+
+    const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID
+      ? process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID
+      : "";
+    const templateID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID
+      ? process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID
+      : "";
+    const userID = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
+      ? process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
+      : "";
+
+    try {
+      const emailParams = {
+        name: userInput.name,
+        email: userInput.email,
+        message: userInput.message,
+      };
+
+      const res = await emailjs.send(
+        serviceID,
+        templateID,
+        emailParams,
+        userID,
+      );
+
+      if (res.status === 200) {
+        console.log("Message sent successfully!");
+        setUserInput({
+          name: "",
+          email: "",
+          message: "",
+        });
+      }
+    } catch (error) {
+      console.error("Failed to send message. Please try again later.", error);
+    }
+  };
 
   return (
     <>
@@ -21,16 +76,37 @@ export default function Contact() {
           lesko.dan.m@gmail.com
         </a>
       </p>
-      <p className="text-lg mt-2 w-1/2 h-1/2">
-        Or through the web here:
-        <Textarea
+      <p className="text-lg mt-2">Or through the web here:</p>
+      <form className="text-lg mt-2 w-1/2 h-1/2" onSubmit={handleSubmit}>
+        <Input
+          value={userInput.name}
+          type="text"
+          name="name"
+          color="purple"
+          placeholder="Name"
           className="bg-zinc-800 text-zinc-300 focus:bg-zinc-800 focus:text-zinc-300 focus:ring-zinc-800 focus:ring-0 focus:ring-offset-0"
+          onChange={handleChange}
+        />
+        <Input
+          value={userInput.email}
+          type="email"
+          name="email"
+          color="purple"
+          placeholder="Email"
+          className="bg-zinc-800 text-zinc-300 focus:bg-zinc-800 focus:text-zinc-300 focus:ring-zinc-800 focus:ring-0 focus:ring-offset-0 mt-1"
+          onChange={handleChange}
+        />
+        <Textarea
+          className="bg-zinc-800 text-zinc-300 focus:bg-zinc-800 focus:text-zinc-300 focus:ring-zinc-800 focus:ring-0 focus:ring-offset-0 mt-1"
           tone="solid"
           color="purple"
           placeholder="Ask me anything!"
-          onChange={(e) => setContactValue(e.target.value)}
+          name="message"
+          value={userInput.message}
+          onChange={handleChange}
         />
-      </p>
+        <Button type="submit">Send Message</Button>
+      </form>
     </>
   );
 }
